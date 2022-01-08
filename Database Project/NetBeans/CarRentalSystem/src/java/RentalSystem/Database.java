a6a2860ca00b43096ecfe5106cc373a5cececcce
hive1181 <himanshuver18@gmail.com>
2022-01-08 16:06:27 +0530

    updated databse

--- Database Project/NetBeans/CarRentalSystem/src/java/RentalSystem/Database.java
+++ Database Project/NetBeans/CarRentalSystem/src/java/RentalSystem/Database.java
@@ -5,7 +5,7 @@ import java.sql.Connection;
 import java.sql.Date;
 import java.sql.SQLException;
 import java.sql.Types;
-
+// hello there some updates
 public class Database
 {
     static DatabaseConnection dbconn = new DatabaseConnection();
